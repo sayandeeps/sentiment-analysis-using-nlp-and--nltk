98209c50bf54bc32c5f5ddb47c7b684c318c4fe5
Sayandeep <sayandeep11@gmail.com>
2023-11-16 20:03:54 +0530

    tokeniation and remove stop words

--- main.py
+++ main.py
@@ -12,6 +12,27 @@ lower_text = text.lower()
 # str2 is list of characters with which the characters need to be replaced
 # str3 is list of characters that needed to be deleted
 clean_text = lower_text.translate(str.maketrans('','',string.punctuation)) # makes a transition table
-print(clean_text)
+
+#tokenization is was of breaking a sentence into words
+tokenized_word = clean_text.split()
+# print(tokenized_word)
+#stop words adds no meaning to the sentence
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+#remove stop wordws
+final_words = []
+for word in tokenized_word:
+    if word not in stop_words:
+        final_words.append(word)
+print(final_words)
 
 
